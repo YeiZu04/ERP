bcf27a4f84a8b1ab016326b444febb41538aff62
GamalielRV <gamaro968@gmail.com>
2024-09-19 16:32:37 -0600

    fix response the employeed

--- ERP-API/Controllers/EmployeeController.cs
+++ ERP-API/Controllers/EmployeeController.cs
@@ -19,28 +19,24 @@ public class EmployeeController : ControllerBase
     public async Task<IActionResult> RegisterEmployee([FromBody] ReqEmployeeDto employeeDto)
     {
         // Llama al servicio para registrar al empleado
-        var result = await _employeeService.RegisterEmployeeAsync(employeeDto);
+        var response = await _employeeService.RegisterEmployeeAsync(employeeDto);
 
         // Verifica si la operación fue exitosa
-        if (result.Success)
+        if (response.Success)
         {
             // Devuelve una respuesta 200 OK con el ID del empleado registrado
-            return Ok(new
-            {
-                message = "Empleado registrado exitosamente",
-                employeeId = result.Data
-            });
+            return Ok(response);
         }
         else
         {
             // Devuelve una respuesta con el código de error adecuado
-            return result.ErrorCode switch
+            return response.ErrorCode switch
           {
-              Api_Response.ErrorCode.UserAlreadyExists => Conflict(new { message = result.ErrorMessage }),
-              Api_Response.ErrorCode.InvalidInput => BadRequest(new { message = result.ErrorMessage }),
-              Api_Response.ErrorCode.NotFound => NotFound(new { message = result.ErrorMessage }),
-              Api_Response.ErrorCode.errorDataBase => StatusCode(500, new { message = result.ErrorMessage }),
-              _ => StatusCode(500, new { message = result.ErrorMessage })
+              Api_Response.ErrorCode.UserAlreadyExists => Conflict(new { message = response.ErrorMessage }),
+              Api_Response.ErrorCode.InvalidInput => BadRequest(new { message = response.ErrorMessage }),
+              Api_Response.ErrorCode.NotFound => NotFound(new { message = response.ErrorMessage }),
+              Api_Response.ErrorCode.errorDataBase => StatusCode(500, new { message = response.ErrorMessage }),
+              _ => StatusCode(500, new { message = response.ErrorMessage })
           };
         }
     }
